956ee4200612152d03d010d4ee45248ce0b1aa36
Bin Chen <pierr.chen@gmail.com>
2025-05-05 21:33:01 +1000

    fix npm build error

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,7 +1,9 @@
 import { useRouter } from 'next/router';
 import SettingsModal from '@/components/SettingsModal';
+import { useState } from 'react';
 
 export default function SettingsPage() {
     const router = useRouter();
-    return <SettingsModal onClose={() => router.push('/')} />;
+    const [showKeyboard, setShowKeyboard] = useState(true);
+    return <SettingsModal onClose={() => router.push('/')} showKeyboard={showKeyboard} setShowKeyboard={setShowKeyboard} />;
 } 
